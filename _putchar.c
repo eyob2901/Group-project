5a34caee8aa4e793f958851203bf8ff1d1c59bf0
Eyob Eshetu <eyob2901@gmail.com>
2022-07-12 06:22:59 -0700

    _putchar fix

--- _putchar.c
+++ _putchar.c
@@ -2,27 +2,27 @@
 
 /**
  * _putchar - print char with stdout
- * @ch: char to print
+ * @c: char to print
  * Return: Output.
  */
-int _putchar(char ch)
+int _putchar(char c)
 {
 static int contador;
 static char buffer[1024];
 
-if (ch == -1)
+if (c == -1)
 {
 contador = 0;
 return (0);
 }
-if (ch == -2 || contador == 1024)
+if (c == -2 || contador == 1024)
 {
 write(1, buffer, contador);
 contador = 0;
 }
-if (ch != -1 && ch != -2)
+if (c != -1 && c != -2)
 {
-buffer[contador] = ch;
+buffer[contador] = c;
 contador++;
 return (1);
 }
