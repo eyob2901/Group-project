7ccf3ef3456365def5cea15186a254d610b42985
Eyob Eshetu <eyob2901@gmail.com>
2022-07-12 06:33:48 -0700

    printf fix

--- _putchar.c
+++ _putchar.c
@@ -1,30 +1,12 @@
 #include <unistd.h>
-
 /**
- * _putchar - print char with stdout
- * @c: char to print
- * Return: Output.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-static int contador;
-static char buffer[1024];
-
-if (c == -1)
-{
-contador = 0;
-return (0);
-}
-if (c == -2 || contador == 1024)
-{
-write(1, buffer, contador);
-contador = 0;
-}
-if (c != -1 && c != -2)
-{
-buffer[contador] = c;
-contador++;
-return (1);
-}
-return (0);
+	return (write(1, &c, 1));
 }
