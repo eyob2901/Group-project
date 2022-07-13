fd22d8ca5dc7bde55563f54ccc285900d3029396
Eyob Eshetu <eyob2901@gmail.com>
2022-07-13 02:32:30 -0700

    eyob

---
+++ Test/test.c
@@ -0,0 +1,19 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * return: always 0
+ */
+
+int main(void)
+
+{
+
+int n = printf("Eyob %c", 'E');
+
+printf("\nn = %d \n", n);
+
+return (0);
+
+}
